49ad2b12257a3df7b2becd436fa901bbdae8c4d1
Vincent Balbarin <vbalbarin@microsoft.com>
2025-08-04 16:01:44 +0000

    Fixing bug in getting path of template files

--- scripts/New-PolicySourceDir.ps1
+++ scripts/New-PolicySourceDir.ps1
@@ -20,30 +20,15 @@ $replacementTable = @{
 $policySrcDir = New-Item -ItemType Directory -Path $PolicyName
 $policySrcDirPath = $policySrcDir.FullName
 
-# $policyMetadata = @"
-# {
-#   "version": "0.0.1",
-#   "guid": "$id"    
-# }
-# "@
+$templateFiles = Get-ChildItem -Path "$scriptDirPath\templates" -Filter "*.txt"
 
-# New-Item -Path "$policySrcDirPath\metadata.jsonc" -ItemType File -Value $policyMetadata
-
-# $newPackage = Get-Content -Path "$scriptDirPath\templates\New-Package.ps1.txt" -Raw
-# $newPolicytemplates = Get-Content -Path "$scriptDirPath\templates\New-Policy.ps1.txt" -Raw
-
-# New-Item -Path "$policySrcDirPath\New-Package.ps1" -ItemType File -Value $newPackage.Replace('${{PolicyName}}', $PolicyName)
-# New-Item -Path "$policySrcDirPath\New-Policy.ps1" -ItemType File -Value $newPolicytemplates.Replace('${{PolicyName}}', $PolicyName)
-
-$templateFile = Get-ChildItem -Path "$scriptDirPath\templates" -Filter "*.txt"
-
-$templateFile | ForEach-Object {
-  $f = $_
-  [string] $contents = Get-Content -Path "$f" -Raw
+$templateFiles | ForEach-Object {
+  $templateFile = $_
+  [string] $contents = Get-Content -Path "$($templateFile.FullName)" -Raw
   $replacementTable.Keys | ForEach-Object {
     # Write-Verbose "Replacing `$`{`{$($_)`}`} with value $($replacementTable[$_])"
     $contents = $contents.Replace("`$`{`{$($_)`}`}", $replacementTable[$_])
   }
-  New-Item -Path "$policySrcDirPath\$($f.BaseName)" -ItemType File -Value $contents
+  New-Item -Path "$policySrcDirPath\$($templateFile.BaseName)" -ItemType File -Value $contents
   
 }
