e812a285aad7f6a972fbd023b9ecf0e450ba89bb
Vincent Balbarin <vbalbarin@microsoft.com>
2025-08-08 00:04:35 +0000

    v0.1.0

--- WindowsServer-2022-MS-2.4/New-Configuration.ps1
+++ WindowsServer-2022-MS-2.4/New-Configuration.ps1
@@ -24,8 +24,38 @@ configuration this
             OsRole      = 'MS'
             OrgSettings  = "../OrgSettings/WindowsServer-2022-MS-2.4.org.xml"
             StigVersion = '2.4'
-            #DomainName  = 'sample.test'
+            #DomainName  = 'wirebyte.local'
             #ForestName  = 'sample.test'
+
+            Exception   = @{
+                'V-254434' = @{ Identity = 'Administrators,Authenticated Users' }
+                'V-254436' = @{ Identity = 'Administrators,Guests' }
+                'V-254437' = @{ Identity = 'Administrators' }
+                'V-254438' = @{ Identity = 'Administrators,Guests' }
+                'V-254439' = @{ Identity = 'Administrators,Local account,Guests' }
+                'V-254440' = @{ Identity = 'NUL' }
+                'V-254491' = @{ Identity = 'NUL' }
+                'V-254492' = @{ Identity = 'NUL' }
+                # 'V-254493' = @{ Identity = 'Administrators' }
+                # 'V-254494' = @{ Identity = 'Administrators' }
+                # 'V-254495' = @{ Identity = 'Administrators' }
+                'V-254496' = @{ Identity = 'NUL' }
+                # 'V-254497' = @{ Identity = 'Administrators,Service,Local Service,Network Service' }
+                'V-254498' = @{ Identity = 'NUL' }
+                # 'V-254500' = @{ Identity = 'Administrators' }
+                # 'V-254501' = @{ Identity = 'Administrators' }
+                # 'V-254502' = @{ Identity = 'Local Service,Network Service' }
+                # 'V-254503' = @{ Identity = 'Administrators,Service,Local Service,Network Service' }
+                # 'V-254504' = @{ Identity = 'Administrators' }
+                # 'V-254505' = @{ Identity = 'Administrators' }
+                'V-254506' = @{ Identity = 'NUL' }
+                # 'V-254507' = @{ Identity = 'Administrators' }
+                # 'V-254508' = @{ Identity = 'Administrators' }
+                # 'V-254509' = @{ Identity = 'Administrators' }
+                # 'V-254510' = @{ Identity = 'Administrators' }
+                # 'V-254511' = @{ Identity = 'Administrators' }
+                # 'V-254512' = @{ Identity = 'Administrators' }
+            }
         }
     }
 }
